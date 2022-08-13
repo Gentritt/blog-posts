12564021026979d05cdc4ad089b31b052b3c956f
Gentrit Selimi <gentrit.selimi@solaborate.com>
2022-08-13 18:24:02 +0200

    Implementation of routing in pages

--- src/App.js
+++ src/App.js
@@ -2,33 +2,53 @@ import React from "react";
 import SearchInput from "./SearchInput";
 import axios from "axios";
 import ImageList from "./ImageList";
+import NavBar from "./NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { images: [] };
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { images: [] };
+//     this.onSearchSubmit = this.onSearchSubmit.bind(this);
+//   }
 
-  async onSearchSubmit(entry) {
-    console.log(entry);
+//   async onSearchSubmit(entry) {
+//     console.log(entry);
 
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=29247103-26b0f9d5bf8b5aa2371184978&q=${entry}&image_type=photo`
-    );
+//     const response = await axios.get(
+//       `https://pixabay.com/api/?key=29247103-26b0f9d5bf8b5aa2371184978&q=${entry}&image_type=photo`
+//     );
 
-    this.setState({ images: response.data.hits });
-    console.log(this.state.images);
-  }
+//     this.setState({ images: response.data.hits });
+//     console.log(this.state.images);
+//   }
 
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "30px" }}>
-        <SearchInput onSearchSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div className="ui container" style={{ marginTop: "30px" }}>
+//         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+//         <ImageList images={this.state.images} />
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      className="ui raised very padded text container segment"
+      style={{ marginTop: "80px" }}
+    >
+      <h1 className="ui header">Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus
+        lorem vel neque gravida, ac dapibus dolor condimentum. Etiam sed felis
+        nunc. Cras pulvinar tortor vitae aliquet suscipit. Suspendisse vehicula
+        molestie orci, vitae rhoncus massa facilisis lobortis. In turpis justo,
+        hendrerit vel lacinia vitae, euismod sit amet est. Aenean vestibulum,
+        orci ac hendrerit maximus, dolor risus feugiat urna, eu rhoncus lorem ex
+        a orci. Etiam id maximus neque. Donec posuere lectus a tortor placerat
+        sodales. Phasellus semper laoreet suscipit. Pellentesque finibus
+        elementum quam eget sagittis. Nam interdum purus metus, ac sodales
+        tortor fermentum vel.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
