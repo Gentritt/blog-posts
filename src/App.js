dd71c52fab2188891b5e6027ca2a36c668bda2f0
Gentrit Selimi <gentrit.selimi@solaborate.com>
2022-08-13 17:31:43 +0200

    implmentation of search input and calling an api to return images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import SearchInput from "./SearchInput";
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "30px" }}>
-      <SearchInput />
-    </div>
-  );
-};
+import axios from "axios";
+import ImageList from "./ImageList";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+
+  async onSearchSubmit(entry) {
+    console.log(entry);
+
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=29247103-26b0f9d5bf8b5aa2371184978&q=${entry}&image_type=photo`
+    );
+
+    this.setState({ images: response.data.hits });
+    console.log(this.state.images);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "30px" }}>
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { entry: "" };
+  }
+
+  //  onInputChange(event) {
+  //     this.setState({ entry: event.target.value });
+  //   }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSearchSubmit(this.state.entry);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" action="">
+        <form onSubmit={this.onFormSubmit} className="ui form" action="">
           <div className="ui massive icon input">
-            <input type="text" placeholder="search" />
+            <input
+              type="text"
+              placeholder="search"
+              onChange={event => this.setState({ entry: event.target.value })}
+              value={this.state.entry}
+            />
             <i className="search icon"></i>
           </div>
         </form>
